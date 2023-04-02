bc7ba84d2af51fedfe746a5e154b1c7b13927c43
Gokhan <engokhangok@gmail.com>
2023-04-02 03:26:08 +0300

    Migrationlar ve Veri Tabanı İlişkileri Eklendi

--- DataAccess/Migrations/20230402002107_mig_AddedRelationships2.Designer.cs
+++ DataAccess/Migrations/20230402002107_mig_AddedRelationships2.Designer.cs
@@ -4,14 +4,16 @@ using DataAccess.Concrete.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230402002107_mig_AddedRelationships2")]
+    partial class mig_AddedRelationships2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
