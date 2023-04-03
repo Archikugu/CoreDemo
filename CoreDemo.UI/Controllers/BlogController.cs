879db6ebd5cc0b39c8705d62dbec4190eeea5da1
Gokhan <engokhangok@gmail.com>
2023-04-03 17:20:14 +0300

    Blogların Listelenmesi Eklendi

--- CoreDemo.UI/Controllers/BlogController.cs
+++ CoreDemo.UI/Controllers/BlogController.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.UI.Controllers
 {
-    public class BlogController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class BlogController : Controller
+	{
+		BlogManager blogManager = new BlogManager(new EfBlogDal());
+		public IActionResult Index()
+		{
+			var values = blogManager.TGetList();
+			return View(values);
+		}
+	}
 }
