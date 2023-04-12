241d3650ab23f7a7f56cb63e15af0ffea6e2f991
Gokhan <engokhangok@gmail.com>
2023-04-12 16:55:03 +0300

    Yazar kayıt sayfası ve sweet alert eklendi

---
+++ CoreDemo.UI/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.UI.Controllers
+{
+	public class RegisterController : Controller
+	{
+		WriterManager writerManager = new WriterManager(new EfWriterDal());
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Index(Writer writer)
+		{
+			writer.WriterStatus = true;
+			writer.WriterAbout = "DenemeTest";
+			writerManager.WriterAdd(writer);
+			return RedirectToAction("Index", "Blog");
+		}
+
+	}
+}
