924acdd55149fce0a105ba658d94b58cd5e9ab93
Gokhan <engokhangok@gmail.com>
2023-04-15 18:49:01 +0300

    Fluent Validation Core Eklendi

---
+++ Business/ValidationRules/WriterValidator.cs
@@ -0,0 +1,38 @@
+﻿using Entity.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Lütfen Yazar Adını Boş Bırakmayınız!");
+
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Lütfen Hakkımda kısmını Boş Bırakmayınız!");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karakterlik bir Ad yazınız!");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre Boş Geçilemez.");
+            RuleFor(x => x.WriterMail).EmailAddress().WithMessage("Mail Adresiniz example@example.com Şeklinde Olmalıdır.");
+            RuleFor(w => w.WriterPassword).Must(IsPasswordValid).WithMessage("Parolanızda en az bir küçük harf bir büyük harf ve rakam olmalıdır!");
+
+        }
+        private bool IsPasswordValid(string arg)
+        {
+            try
+            {
+                Regex regex = new Regex(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[0-9])[A-Za-z\d]");
+                return regex.IsMatch(arg);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- CoreDemo.UI/Controllers/RegisterController.cs
+++ CoreDemo.UI/Controllers/RegisterController.cs
@@ -1,27 +1,42 @@
 ﻿using Business.Concrete;
+using Business.ValidationRules;
 using DataAccess.EntityFramework;
 using Entity.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.UI.Controllers
 {
-	public class RegisterController : Controller
-	{
-		WriterManager writerManager = new WriterManager(new EfWriterDal());
+    public class RegisterController : Controller
+    {
+        WriterManager writerManager = new WriterManager(new EfWriterDal());
 
-		[HttpGet]
-		public IActionResult Index()
-		{
-			return View();
-		}
-		[HttpPost]
-		public IActionResult Index(Writer writer)
-		{
-			writer.WriterStatus = true;
-			writer.WriterAbout = "DenemeTest";
-			writerManager.WriterAdd(writer);
-			return RedirectToAction("Index", "Blog");
-		}
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Writer writer)
+        {
+            WriterValidator writerValidator = new WriterValidator();
+            ValidationResult result = writerValidator.Validate(writer);
+            if (result.IsValid)
+            {
+                writer.WriterStatus = true;
+                writer.WriterAbout = "DenemeTest";
+                writerManager.WriterAdd(writer);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
 
-	}
+    }
 }
