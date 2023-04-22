7ba457954cc86fdd6f4c40968310561ba5d48918
Gokhan <engokhangok@gmail.com>
2023-04-22 23:57:53 +0300

    Mail Bülteni Eklendi

---
+++ Business/Concrete/NewsletterManager.cs
@@ -0,0 +1,52 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class NewsletterManager : INewsletterService
+    {
+        INewsletterDal _newsletterDal;
+
+        public NewsletterManager(INewsletterDal newsletterDal)
+        {
+            _newsletterDal = newsletterDal;
+        }
+
+        public void TAdd(Newsletter t)
+        {
+            _newsletterDal.Insert(t);
+        }
+
+        public void TDelete(Newsletter t)
+        {
+            _newsletterDal.Delete(t);
+        }
+
+        public List<Newsletter> TGetByFilter(Expression<Func<Newsletter, bool>> filter)
+        {
+            return _newsletterDal.GetListAll(filter);
+        }
+
+        public Newsletter TGetByID(int id)
+        {
+            return _newsletterDal.GetById(id);
+        }
+
+        public List<Newsletter> TGetList()
+        {
+            return _newsletterDal.GetListAll();
+        }
+
+        public void TUpdate(Newsletter t)
+        {
+            _newsletterDal.Update(t);
+        }
+    }
+}
---
+++ CoreDemo.UI/Controllers/NewsletterController.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.UI.Controllers
+{
+    public class NewsletterController : Controller
+    {
+        NewsletterManager newsletterManager = new NewsletterManager(new EfNewsletterDal());
+        [HttpGet]
+        public PartialViewResult SubscribeMail()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult SubscribeMail(Newsletter newsletter)
+        {
+            newsletter.MailStatus = true;
+            newsletterManager.TAdd(newsletter);
+            return PartialView();
+        }
+    }
+}
