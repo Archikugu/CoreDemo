5e2fb557e81b6b2ff406b1ce9985ea2eb1d317a0
Gokhan <engokhangok@gmail.com>
2023-05-06 21:59:42 +0300

    Error 404 sayfası eklendi ve düzenlemeler yapıldı.

---
+++ CoreDemo.UI/Controllers/ErrorPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.UI.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        public IActionResult Error404(int code)
+        {
+            return View();
+        }
+    }
+}
