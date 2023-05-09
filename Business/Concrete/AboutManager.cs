a603317b452d771145e120d6b1317b4d1543d5ac
Gokhan <engokhangok@gmail.com>
2023-05-09 16:18:01 +0300

    About İşlemleri Eklendi

---
+++ Business/Concrete/AboutManager.cs
@@ -0,0 +1,52 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+
+        public void TAdd(About t)
+        {
+            _aboutDal.Insert(t);
+        }
+
+        public void TDelete(About t)
+        {
+            _aboutDal.Delete(t);
+        }
+
+        public List<About> TGetByFilter(Expression<Func<About, bool>> filter)
+        {
+            return _aboutDal.GetListAll(filter);
+        }
+
+        public About TGetByID(int id)
+        {
+            return _aboutDal.GetById(id);
+        }
+
+        public List<About> TGetList()
+        {
+            return _aboutDal.GetListAll();
+        }
+
+        public void TUpdate(About t)
+        {
+            _aboutDal.Update(t);
+        }
+    }
+}
---
+++ CoreDemo.UI/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.UI.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutDal());
+        public IActionResult Index()
+        {
+            var values = aboutManager.TGetList();
+            return View(values);
+        }
+        public PartialViewResult SocialMediaAbout()
+        {
+            return PartialView();
+        }
+    }
+}
