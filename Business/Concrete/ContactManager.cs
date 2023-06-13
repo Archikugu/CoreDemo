15ab1409cab9cd4793a8a797928dff9660bf659a
Gokhan <engokhangok@gmail.com>
2023-06-13 18:22:17 +0300

    Contact Sayfası Oluşturuldu

---
+++ Business/Concrete/ContactManager.cs
@@ -0,0 +1,56 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        IContactDal _contactDal;
+        public ContactManager(IContactDal contactDal)
+        {
+            _contactDal = contactDal;
+        }
+
+        public void ContactAdd(Contact contact)
+        {
+            _contactDal.Insert(contact);
+        }
+
+        public void TAdd(Contact t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Contact t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contact> TGetByFilter(Expression<Func<Contact, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contact> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Contact t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CoreDemo.UI/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CoreDemo.UI.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            contact.ContactDate = DateTime.Parse(DateTime.Now.ToString());
+            contact.ContactStatus = true;
+            contactManager.ContactAdd(contact);
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}
